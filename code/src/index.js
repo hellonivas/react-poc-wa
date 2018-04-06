5fb334cae3171422af0666391aa3e9a08e04b23f
Srinivasan <Srini@Krossark.local>
2018-04-06 17:36:40 +0530

    SASS Implemented Signed-off-by: Srinivasan <Srini>

--- code/src/index.js
+++ code/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/styles/styles.css';
 import { NavBar } from './scenes/sharedComponent';
 
 ReactDOM.render(<NavBar />, document.getElementById('root'));
