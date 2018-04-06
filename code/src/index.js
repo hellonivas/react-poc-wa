16a738fc30db25669294b788036fe9d0a8ed9603
Srinivasan <Srini@Krossark.local>
2018-04-06 17:25:33 +0530

    Folder Structure Changed Signed-off-by: Srinivasan <Srini>

--- code/src/index.js
+++ code/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { NavBar } from './scenes/sharedComponent';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<NavBar />, document.getElementById('root'));
---
+++ code/src/scenes/sharedComponent/navBar/index.js
@@ -0,0 +1,13 @@
+import React, { Component,Fragment } from 'react';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <Fragment>
+                <h1>NavBars</h1>
+            </Fragment>
+        );
+    }
+}
+
+export default NavBar;
